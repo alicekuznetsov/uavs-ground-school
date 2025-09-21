3c039ddf45ccb037ae9a314dcee495e33babd1ff
Alice Kuznetsov <90939494+alicekuznetsov@users.noreply.github.com>
2025-09-20 18:25:31 -0700

    Automatically determine hue masks

--- color_me_impressed.py
+++ color_me_impressed.py
@@ -18,58 +18,56 @@ hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # TODO: Use color distribution (the video has a histogram, thats a pretty good idea) to automatically determine which colors to mask
 
-# Define the range for red in HSV - hue range is [0,179] (360 / 2)
-# Red appears twice in HSV, so we need 2 masks :p
-lower_red1 = (0, 50, 50) 
-upper_red1 = (10, 255, 255)
-lower_red2 = (170, 50, 50)  
-upper_red2 = (180, 255, 255)
+# Calculate the histogram of the hue channel
+hue_channel = hsv_img[0, :, 0]
+hist = cv2.calcHist([hue_channel], [0], None, [180], [0, 180])
 
-lower_blue = (85, 50, 50) 
-upper_blue = (125, 255, 255)
+# Find the peak hues above 30% of the maximum histogram value
+hist_peaks = []
+threshold = hist.max() * 0.3
+for hue, occurrences in enumerate(hist):
+    if occurrences > threshold:
+        # Don't add a hue if its close to another hue
+        print(hue)
+        if len(hist_peaks) == 0 or abs(hist_peaks[len(hist_peaks) - 1] - hue) >= 10:
+            hist_peaks.append(hue)
+            print('Appended', hue)
+        else:
+            print('Modifying', hist_peaks[len(hist_peaks) - 1], 'to', hue )
+            hist_peaks[len(hist_peaks) - 1] = hue
+print("Peaks found at", hist_peaks)
 
-lower_purple = (125, 50, 50) 
-upper_purple = (140, 255, 255)
+hue_masks = []
+for hue in hist_peaks:
+    # Max & Min functions allow for wrapping 
+    lower_bound =  (max(0, hue - 10), 50, 50)
+    upper_bound = (min(179, hue + 10), 255, 255)
+    hue_masks.append(cv2.inRange(hsv_img, lower_bound, upper_bound))
+    
 
-# MASKS: Black-and-white images containing the colors.
-# Create masks for both red ranges and combine them.
-red_mask1 = cv2.inRange(hsv_img, lower_red1, upper_red1)
-red_mask2 = cv2.inRange(hsv_img, lower_red2, upper_red2)
-red_mask = cv2.bitwise_or(red_mask1, red_mask2)
-blue_mask = cv2.inRange(hsv_img, lower_blue, upper_blue)
-purple_mask = cv2.inRange(hsv_img, lower_purple, upper_purple)
-
-# Use masks on original image
-red_img = cv2.bitwise_and(img, img, mask=red_mask)
-blue_img = cv2.bitwise_and(img, img, mask=blue_mask)
-purple_img = cv2.bitwise_and(img, img, mask=purple_mask)
-
-
-# Show the world what you've made :D
-# TODO: Put these in a figure
-
-fig, axs = plt.subplots(ncols=2, nrows=4, figsize=(5.5, 3.5), layout="constrained")
+fig, axs = plt.subplots(ncols=2, nrows=1 + len(hue_masks), figsize=(5.5, 3.5), layout="constrained")
 # Hide axis markers :D
 for ax in axs.flat:
     ax.axis('off')
 axs[0, 1].axis('on')  
 
-# Calculate the histogram of the hue channel
-hue_channel = hsv_img[0, :, :]
-hist = cv2.calcHist([hue_channel], [0], None, [180], [0, 180])
-
-# Plot the histogram
-axs[0, 1].plot(hist, color='orange')
+# -- Hue Histogram :D -- 
+axs[0, 1].plot(hist)
 axs[0, 1].set_title("Hue Distribution")
+axs[0, 1].set_xlabel("Hue")
+axs[0, 1].set_ylabel("Occurrences")
 axs[0, 1].set_xlim([0, 180])
 
+axs[0, 0].set_title("Provided Image")
 axs[0, 0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-axs[1, 0].imshow(cv2.cvtColor(red_img, cv2.COLOR_BGR2RGB))
-axs[1, 1].imshow(cv2.cvtColor(red_mask, cv2.COLOR_BGR2RGB))
-axs[2, 0].imshow(cv2.cvtColor(blue_img, cv2.COLOR_BGR2RGB))
-axs[2, 1].imshow(cv2.cvtColor(blue_mask, cv2.COLOR_BGR2RGB))
-axs[3, 0].imshow(cv2.cvtColor(purple_img, cv2.COLOR_BGR2RGB))
-axs[3, 1].imshow(cv2.cvtColor(purple_mask, cv2.COLOR_BGR2RGB))
+
+# -- Plot histograms and masks -- 
+i = 0
+while i < len(hue_masks):
+    afwadha = cv2.bitwise_and(img, img, mask=hue_masks[i])
+    axs[1 + i, 0].imshow(cv2.cvtColor(afwadha, cv2.COLOR_BGR2RGB))
+    axs[1 + i, 1].imshow(cv2.cvtColor(hue_masks[i], cv2.COLOR_BGR2RGB))
+    i += 1
 
 plt.show()
 
